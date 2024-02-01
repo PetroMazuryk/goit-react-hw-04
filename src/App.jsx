c789869aeb408a174cbd962dcb8c3d2007a96f17
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-02-01 23:15:46 +0200

    fixed http

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { ImageCallery } from './components/ImageGallery/ImageGallery';
 import { SearchBar } from './components/SearchBar/SearchBar';
 import { Toaster } from 'react-hot-toast';
 
-const API_URL = 'https://api.unsplash.com/photos';
+const API_URL = 'https://api.unsplash.com/search/photos';
+const API_KEY = 'IQfqBWFtUwIv7vwsJMmuzblOE_R_YD5Ct0w72vsl7Rw';
 
 const IMAGES_PER_PAGE = 8;
 
@@ -19,12 +20,11 @@ export const App = () => {
       setArticles([]);
       setLoading(true);
       const response = await axios.get(
-        `${API_URL}?query=${query}&per_page=${IMAGES_PER_PAGE}&client_id=${
-          import.meta.env.VITE_API_KEY
-        }`
+        `${API_URL}?page=1&query=${query}&per_page=${IMAGES_PER_PAGE}&client_id=${API_KEY}`
       );
-      console.log(response.data);
-      setArticles(response.data);
+
+      console.log(response.data.results);
+      setArticles(response.data.results);
     } catch (error) {
       setError(true);
     } finally {
