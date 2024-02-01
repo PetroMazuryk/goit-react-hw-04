2c022c0de074cd62c88a0272adc265826aa8e6ce
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-02-02 01:20:13 +0200

    added a button to download more images

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ImageCallery } from './components/ImageGallery/ImageGallery';
 import { SearchBar } from './components/SearchBar/SearchBar';
 import { Toaster } from 'react-hot-toast';
@@ -10,34 +10,54 @@ const API_KEY = 'IQfqBWFtUwIv7vwsJMmuzblOE_R_YD5Ct0w72vsl7Rw';
 const IMAGES_PER_PAGE = 8;
 
 export const App = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const searchArticles = async query => {
-    try {
-      setError(false);
-      setArticles([]);
-      setLoading(true);
-      const response = await axios.get(
-        `${API_URL}?page=1&query=${query}&per_page=${IMAGES_PER_PAGE}&client_id=${API_KEY}`
-      );
-
-      console.log(response.data.results);
-      setArticles(response.data.results);
-    } catch (error) {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
+  const searchArticles = async newQuery => {
+    setQuery(newQuery);
+    setPage(1);
+    setArticles([]);
   };
+  const handleLoadMore = () => {
+    setPage(page + 1);
+  };
+
+  useEffect(() => {
+    if (query === '') {
+      return;
+    }
+
+    async function fetchData() {
+      try {
+        setLoading(true);
+        setError(false);
+        const response = await axios.get(
+          `${API_URL}?page=${page}&query=${query}&per_page=${IMAGES_PER_PAGE}&client_id=${API_KEY}`
+        );
+        setArticles(prevArticles => [...prevArticles, ...response.data.results]);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [query, page]);
 
   return (
     <>
       <SearchBar onSearch={searchArticles} />
-      {loading && <b>Loading articles, please wait ...</b>}
+
       {error && <b>Oops, there was an error, please try reloading 😅</b>}
       {articles.length > 0 && <ImageCallery items={articles} />}
+      {loading && <b>Loading articles, please wait ...</b>}
+      {articles.length > 0 && !articles.loading && (
+        <button onClick={handleLoadMore}>Load more</button>
+      )}
+
       <Toaster position="top-right" />
     </>
   );
