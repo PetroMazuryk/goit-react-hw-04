dc17e1e4b849745aa1728fedc1d71fdc6e5d86ea
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-02-03 16:51:04 +0200

    added component ImageCard

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,15 +1,18 @@
 import css from './ImageCard.module.css';
 
-export const ImageCard = ({ item }) => {
-  <div>
-    <div className={css.thumb}>
-      <img src={item.urls.small} alt={item.description} />
+export const ImageCard = ({ item: { urls, description, user } }) => {
+  return (
+    <div className={css.wrapper}>
+      <div className={css.thumb}>
+        <img className={css.img} src={urls.small} alt={description} />
+      </div>
+      <div className={css.titleWrapper}>
+        <h2>{user.name}</h2>
+        <p className={css.likes}>{user.total_likes}</p>
+        <p>Кількість фотографій: {user.total_photos}</p>
+        <p>Кількість колекцій: {user.total_collections}</p>
+        <p className={css.titleYear}> {user.updated_at}</p>
+      </div>
     </div>
-
-    <h2>{item.user.name}</h2>
-    <p>{item.user.total_likes}</p>
-    <p>Кількість фотографій: {item.user.total_photos}</p>
-    <p>Кількість колекцій: {item.user.total_collections}</p>
-    <p> {item.user.updated_at}</p>
-  </div>;
+  );
 };
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,22 +1,12 @@
 import css from './ImageGallery.module.css';
+import { ImageCard } from '../ImageCard/ImageCard';
 
 export const ImageCallery = ({ items }) => {
   return (
     <ul className={css.list}>
       {items.map(item => (
         <li key={item.user.id}>
-          <div className={css.wrapper}>
-            <div className={css.thumb}>
-              <img className={css.img} src={item.urls.small} alt={item.description} />
-            </div>
-            <div className={css.titleWrapper}>
-              <h2>{item.user.name}</h2>
-              <p className={css.likes}>{item.user.total_likes}</p>
-              <p>Кількість фотографій: {item.user.total_photos}</p>
-              <p>Кількість колекцій: {item.user.total_collections}</p>
-              <p className={css.titleYear}> {item.user.updated_at}</p>
-            </div>
-          </div>
+          <ImageCard item={item} />
         </li>
       ))}
     </ul>
